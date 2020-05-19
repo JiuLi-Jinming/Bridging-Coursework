32881a16501e572cdc271518d439da09be30a6ff
Jiuli <kmzjm98@gmail.com>
2020-05-19 15:21:46 +0800

    web application

--- Bridging_Coursework/settings.py
+++ Bridging_Coursework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vk-!gh$plhx=p#=5wqi3q4-v9ktuqc04icil($bs$q)^r+=%^t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
